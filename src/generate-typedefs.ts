9d2128cc4facbddc331fee80f0e0940ff1809a01
Isabella Skořepová <isabella@skorepova.info>
2018-10-09 21:48:11 +0200

    Sort tables to generate more stable output

--- src/generate-typedefs.ts
+++ src/generate-typedefs.ts
@@ -62,21 +62,22 @@ export const generateTypedefs = async ({ knex, output }: { knex: Knex, output: s
     'object_schema',
     'public',
   )
-  const tables: any = {}
+  const tableMap: any = {}
   for (const col of columns) {
     if (ignoreTable(col.table_name)) continue
-    if (!tables[col.table_name]) tables[col.table_name] = []
-    tables[col.table_name].push(col)
+    if (!tableMap[col.table_name]) tableMap[col.table_name] = []
+    tableMap[col.table_name].push(col)
   }
+  const tables = Object.entries(tableMap).map(([t, c]) => ({ table: t, cols: c })).sort((a,b) => a.table.localeCompare(b.table))
   let types =
     '/* eslint-disable */\n' +
     '// This is automatically generated file. \n' +
     '// Do not edit, it WILL be overwritten.\n\n'
-  for (const [table, cols] of Object.entries(tables)) {
+  for (const {table, cols} of tables) {
     types += `${defForTable(table, cols, elementTypes)}\n\n`
   }
   types += 'export type TableToTypeMap = {'
-  types += Object.keys(tables).reduce((a, b) => `${a}\n  ${b}: ${b}`, '')
+  types += tables.map(t => t.table).reduce((a, b) => `${a}\n  ${b}: ${b}`, '')
   types += '\n}\n'
   const content = await promisify(fs.readFile)(output, 'utf-8')
   if (content !== types) {
