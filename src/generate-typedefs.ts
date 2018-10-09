13b06942cd22cf87bfa2a904cc2f1edb365d9878
Isabella Skořepová <isabella@skorepova.info>
2018-10-09 21:48:26 +0200

    Prettier document

--- src/generate-typedefs.ts
+++ src/generate-typedefs.ts
@@ -18,7 +18,7 @@ function convertType(type: string) {
     text: 'string',
     boolean: 'boolean',
     'double precision': 'number',
-    bigint: 'string',
+    bigint: 'string'
   } as { [key: string]: string })[type]
   if (!ret) {
     throw new Error(`Unknown type ${type}`)
@@ -34,7 +34,7 @@ function typeForColumn(column: any, elementTypes: any[]) {
       et =>
         et.object_name === column.table_name &&
         et.object_type === 'TABLE' &&
-        et.collection_type_identifier === column.dtd_identifier,
+        et.collection_type_identifier === column.dtd_identifier
     )
     ret = `${convertType(t.data_type)}[]`
   } else {
@@ -48,19 +48,25 @@ function defForTable(table: string, columns: any, elementTypes: any[]) {
   return `interface ${table} {\n${columns
     .map(
       (c: any) =>
-        `  ${transformKey(c.column_name)}: ${typeForColumn(c, elementTypes)}`,
+        `  ${transformKey(c.column_name)}: ${typeForColumn(c, elementTypes)}`
     )
     .join('\n')}\n}`
 }
 
-export const generateTypedefs = async ({ knex, output }: { knex: Knex, output: string }) => {
+export const generateTypedefs = async ({
+  knex,
+  output
+}: {
+  knex: Knex
+  output: string
+}) => {
   const columns = await knex('information_schema.columns').where(
     'table_schema',
-    'public',
+    'public'
   )
   const elementTypes = await knex('information_schema.element_types').where(
     'object_schema',
-    'public',
+    'public'
   )
   const tableMap: any = {}
   for (const col of columns) {
@@ -68,12 +74,14 @@ export const generateTypedefs = async ({ knex, output }: { knex: Knex, output: s
     if (!tableMap[col.table_name]) tableMap[col.table_name] = []
     tableMap[col.table_name].push(col)
   }
-  const tables = Object.entries(tableMap).map(([t, c]) => ({ table: t, cols: c })).sort((a,b) => a.table.localeCompare(b.table))
+  const tables = Object.entries(tableMap)
+    .map(([t, c]) => ({ table: t, cols: c }))
+    .sort((a, b) => a.table.localeCompare(b.table))
   let types =
     '/* eslint-disable */\n' +
     '// This is automatically generated file. \n' +
     '// Do not edit, it WILL be overwritten.\n\n'
-  for (const {table, cols} of tables) {
+  for (const { table, cols } of tables) {
     types += `${defForTable(table, cols, elementTypes)}\n\n`
   }
   types += 'export type TableToTypeMap = {'
