dd8586182fcc35b3460a465763bd47dfba1ce820
Isabella Skořepová <isabella@skorepova.info>
2018-10-10 23:44:37 +0200

    Remove datetime and duration converter

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from './make-loader-maker'
 export * from './generate-typedefs'
 // This is built-in converter. Not neccessary to export
 // export * from './converters/id-converter'
-export * from './converters/date-time'
 export * from './converters/nullable'
