18713b97af2e64bc4b775dd64268f0a809a1dc65
Isabella Skořepová <isabella@skorepova.info>
2019-04-10 15:04:48 +0200

    Export convertToDb - usefull for raw queries

--- src/table-loader.ts
+++ src/table-loader.ts
@@ -508,6 +508,7 @@ export default class TableLoader<
       all: this.all(),
       delete: this.delete(),
       count: this.count(),
+      convertToDb: (v: JSType) => this.toDB(v),
     }
   }
 }
