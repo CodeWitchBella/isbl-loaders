45ef9994e42c6db48d7fd61fa00601edb3849a31
Isabella Skořepová <isabella@skorepova.info>
2019-04-29 16:24:03 +0200

    Finish automatic converter story

--- src/converters/enum-converter.ts
+++ src/converters/enum-converter.ts
@@ -1,10 +1,14 @@
-import { Converter } from '../make-loader-maker'
+import { Converter, ConverterInfo } from '../make-loader-maker'
 
 export const enumConverter = <T extends string>(
   map: { [key in T]: number },
-) => (): Converter<number, T> => {
+) => (info: ConverterInfo<any>): Converter<number, T> => {
   const inverseMap: { [k: number]: T } = {}
   for (const key of Object.keys(map)) {
+    if (map[key as T] in inverseMap)
+      throw new Error(
+        'Duplicate enum value ' + map[key as T] + ' on table ' + info.table,
+      )
     inverseMap[map[key as T]] = key
   }
   return {
--- src/make-loader-maker.ts
+++ src/make-loader-maker.ts
@@ -1,7 +1,8 @@
 import Knex from 'knex'
-import { PickExcept } from '@codewitchbella/ts-utils'
+import { PickExcept, notNull } from '@codewitchbella/ts-utils'
 import mapValues from 'lodash.mapvalues'
 import TableLoader, { InitLoader } from './table-loader'
+import { enumConverter } from './converters/enum-converter'
 
 type Args<FilterArg> = FilterArg extends undefined
   ? { knex: Knex; filterArg?: FilterArg }
@@ -31,6 +32,18 @@ type JSTypeWithID<JSType, TableName> = PickExcept<JSType, 'id'> & {
 export const convertersSymbol = Symbol('converters')
 export const tableLoaderSymbol = Symbol('tableLoader')
 
+function fromEntries(iterable: any) {
+  return [...iterable].reduce(
+    (obj, { 0: key, 1: val }) => Object.assign(obj, { [key]: val }),
+    {},
+  )
+}
+
+type Codegen = {
+  converters: {
+    [key: string]: { [key: string]: any } | undefined
+  }
+}
 export const makeLoaderMaker = <
   Definitions extends {
     table: {}
@@ -38,7 +51,9 @@ export const makeLoaderMaker = <
     insert: { [tab in keyof Definitions['table']]: any }
   },
   FilterArg = undefined
->() => <Table extends keyof Definitions['table']>(opts: {
+>(
+  codegen: Codegen,
+) => <Table extends keyof Definitions['table']>(opts: {
   table: Table
   converters?: {
     [key in keyof Definitions['table'][Table]]?: key extends keyof Definitions['js'][Table]
@@ -71,51 +86,80 @@ export const makeLoaderMaker = <
   }: {
     filter?: (v: Definitions['js'][Table], a: FilterArg) => boolean
   } = {},
-) => (args: Args<FilterArg>) => {
+) => {
   const { onUpdate, onInsert } = opts
-  const converters = mapValues(opts.converters, c =>
-    c ? c({ table: opts.table }) : null,
+
+  const automaticConverters = (() => {
+    const src = codegen.converters[opts.table as string]
+    if (!src) return {}
+    return fromEntries(
+      Object.entries(src)
+        .map(([column, definition]) => {
+          if (
+            typeof definition !== 'object' ||
+            !definition ||
+            !definition.autoConvert
+          )
+            return null
+          if (definition.type === 'enum') {
+            return [column, enumConverter(definition.values)]
+          }
+          throw new Error(
+            `Unknown automatic converter type ${JSON.stringify(
+              definition.type,
+            )}`,
+          )
+        })
+        .filter(notNull),
+    )
+  })()
+  console.log(automaticConverters)
+  const converters = mapValues(
+    { ...opts.converters, ...automaticConverters },
+    c => (c ? c({ table: opts.table }) : null),
   )
-  const loader = new TableLoader<
-    {
-      js: Definitions['js'][Table]
-      table: Definitions['table'][Table]
-      insert: Definitions['insert'][Table]
-    },
-    Table
-  >({
-    toDB: mapValues(converters, v => (v ? v.toDB : null)) as any,
-    fromDB: mapValues(converters, v => (v ? v.fromDB : null)) as any,
-    table: opts.table as string,
-    knex: args.knex,
-    filter: filter ? v => filter(v, args.filterArg!) : undefined,
-    onInsert: (ids: number[]) => {
-      if (onInsert)
-        setImmediate(() => {
-          onInsert(ids.map(id => ({ type: opts.table, id })), args)
-        })
-    },
-    onUpdate: (ids: number[]) => {
-      if (onUpdate)
-        setImmediate(() => {
-          onUpdate(ids.map(id => ({ type: opts.table, id })), args)
-        })
-    },
-  })
+  return (args: Args<FilterArg>) => {
+    const loader = new TableLoader<
+      {
+        js: Definitions['js'][Table]
+        table: Definitions['table'][Table]
+        insert: Definitions['insert'][Table]
+      },
+      Table
+    >({
+      toDB: mapValues(converters, v => (v ? v.toDB : null)) as any,
+      fromDB: mapValues(converters, v => (v ? v.fromDB : null)) as any,
+      table: opts.table as string,
+      knex: args.knex,
+      filter: filter ? v => filter(v, args.filterArg!) : undefined,
+      onInsert: (ids: number[]) => {
+        if (onInsert)
+          setImmediate(() => {
+            onInsert(ids.map(id => ({ type: opts.table, id })), args)
+          })
+      },
+      onUpdate: (ids: number[]) => {
+        if (onUpdate)
+          setImmediate(() => {
+            onUpdate(ids.map(id => ({ type: opts.table, id })), args)
+          })
+      },
+    })
 
-  const custom = definition(loader)
-  const ret = Object.assign(loader.initLoader(), definition(loader))
+    const custom = definition(loader)
+    const ret = Object.assign(loader.initLoader(), definition(loader))
 
-  return Object.assign(ret, {
-    [convertersSymbol]: converters,
-    [tableLoaderSymbol]: loader,
-  }) as (InitLoader<
-    {
-      js: Definitions['js'][Table]
-      table: Definitions['table'][Table]
-      insert: Definitions['insert'][Table]
-    },
-    Table
-  > &
-    typeof custom)
+    return Object.assign(ret, {
+      [convertersSymbol]: converters,
+      [tableLoaderSymbol]: loader,
+    }) as (InitLoader<
+      {
+        js: Definitions['js'][Table]
+        table: Definitions['table'][Table]
+        insert: Definitions['insert'][Table]
+      },
+      Table
+    > &
+      typeof custom)
+  }
 }
